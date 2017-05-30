4851f19342cb5138150cb893f0fa3e9dd02bc300
申嵘涛 <slsoft_srt@foxmail.com>
2017-05-30 17:24:28 +0800

    add master3.java

---
+++ master3.java
@@ -0,0 +1,14 @@
+public static void main(String[] args)
+{
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+	System.out.println("Hello Sealine!");
+
+}
