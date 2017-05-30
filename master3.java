0e0d1b33592a6302b47c26c487e835a74e841106
申嵘涛 <slsoft_srt@foxmail.com>
2017-05-30 17:29:33 +0800

    test stash

--- master3.java
+++ master3.java
@@ -1,14 +1,4 @@
 public static void main(String[] args)
 {
 	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-	System.out.println("Hello Sealine!");
-
 }
